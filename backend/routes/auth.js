6b70d05d4e80cdba0e82bf90ec146866cb34f4c1
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-11-21 13:38:12 +0200

    added auth file for user authentication

---
+++ backend/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const passport = require("passport");
+
+const router = express.Router();
+
+// Google Auth Route
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+// Google Auth Callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    res.redirect("http://localhost:3000");
+  }
+);
+
+// Logout Route
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("http://localhost:3000");
+});
+
+module.exports = router;
